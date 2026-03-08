0ad46ffab8854d8641e9dfda0f336d070652f71d
goentry <glebyudanov1@yandex.ru>
2026-03-08 22:22:17 +0500

    feat/vite fix3

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,7 @@ import Together from "../pages/Together.vue"
 import Letter from "../pages/Letter.vue"
 
 const router = createRouter({
-  history: createWebHistory('/love-site/'),
+  history: createWebHistory(),
   routes: [
     { path: "/", component: Home },
     { path: "/story", component: Story },
